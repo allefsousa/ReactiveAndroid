a5f9ac42293b514a7faa55080e02fea532b77fd2
Allef sousa <allefsousa_1@hotmail.com>
2018-06-23 10:42:59 -0300

    primeiros passos com Observer e Observable

--- app/src/main/java/br/com/developers/allefsousa/rxandroid/PrincipalActivity.java
+++ app/src/main/java/br/com/developers/allefsousa/rxandroid/PrincipalActivity.java
@@ -2,12 +2,53 @@ package br.com.developers.allefsousa.rxandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class PrincipalActivity extends AppCompatActivity {
+    Observable<String> animalsObservable = Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+    private String TAG = "Allef";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+        Observer<String> animalsObserver = getAnimalsObserver();
+
+        animalsObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);
+
+    }
+
+    private Observer<String> getAnimalsObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
     }
 }
